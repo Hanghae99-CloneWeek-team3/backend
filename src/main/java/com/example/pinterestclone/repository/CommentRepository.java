9b1359c2c976f34f2d2475d6deeb10444e8c35d8
DESKTOP-PE8J3UP\주은 <4708408@naver.com>
2022-11-03 00:29:13 +0900

    Fix: repository 수정

--- src/main/java/com/example/pinterestclone/repository/CommentRepository.java
+++ src/main/java/com/example/pinterestclone/repository/CommentRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
    @Query(" SELECT c FROM Comment c WHERE c.post.id = :postId and c.rootName = :rootName ")
